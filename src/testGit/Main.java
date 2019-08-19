2493828212985c804a0699cbf977114dc55807fb
AndreiSolomatin <solo-msu@yandex.ru>
2019-08-19 19:36:24 +0400

    Add new line

---
+++ src/testGit/Main.java
@@ -0,0 +1,9 @@
+package testGit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("I came into this world!");
+        System.out.println("Commit testing #1.");
+    }
+}
