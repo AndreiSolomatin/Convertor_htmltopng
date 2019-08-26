16e966a0eae32a0318a73bc8add7470601899ef7
AndreiSolomatin <solo-msu@yandex.ru>
2019-08-26 23:51:07 +0400

    Remove tem project

--- src/testGit/Main.java
+++
@@ -1,9 +0,0 @@
-package testGit;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("I came into this world!");
-        System.out.println("Commit testing #1.");
-    }
-}
